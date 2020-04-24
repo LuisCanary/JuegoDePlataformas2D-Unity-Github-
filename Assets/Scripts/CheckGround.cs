6241eb89d16bf41146d36817882d28138217655c
Luis <luisgomezruiz@gmail.com>
2020-04-24 16:24:30 +0200

    Animations done

---
+++ Assets/Scripts/CheckGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+	public static bool isGrounded;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		isGrounded = true;
+		Debug.Log("Esta en el suelo");
+	}
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		isGrounded = false;
+		Debug.Log("NO Esta en el suelo");
+
+	}
+}
